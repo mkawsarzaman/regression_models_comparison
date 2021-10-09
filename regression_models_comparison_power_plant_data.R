4a6a34c1ee29222afab1d88ef021bb069855dbcc
mkawsarzaman <mkawsarzaman@mun.ca>
2021-10-09 19:43:40 -0230

    Random forest regressor with the best result

--- regression_models_comparison_power_plant_data.R
+++ regression_models_comparison_power_plant_data.R
@@ -14,7 +14,7 @@ test_set = subset(dataset, split == FALSE)
 #multiple linear regression
 
 lin_reg = lm(formula = PE ~ .,
-             data = dataset)
+             data = training_set)
 y_pred = predict(object = lin_reg, newdata = test_set)
 summary(lin_reg)
 
@@ -39,7 +39,7 @@ dataset$AP3 = dataset$AP^3
 dataset$RH3 = dataset$RH^3
 
 poly_reg = lm(formula = PE ~ .,
-              data = dataset)
+              data = training_set)
 y_pred = predict(object = poly_reg, newdata = test_set)
 summary(poly_reg)
 
@@ -57,7 +57,7 @@ test_set = subset(dataset, split == FALSE)
 # install.packages('e1071')
 library(e1071)
 svr = svm(formula = PE ~ ., 
-          data = dataset,
+          data = training_set,
           type = 'eps-regression',
           kernel = 'radial')
 y_pred = predict(object = svr, newdata = test_set)
@@ -76,11 +76,28 @@ test_set = subset(dataset, split == FALSE)
 library(rpart)
 
 dt_regressor = rpart(formula = PE ~ .,
-                  data = dataset,
+                  data = training_set,
                   control = rpart.control(minsplit = 1))
 y_pred = predict(object = dt_regressor, newdata = test_set)
 summary(dt_regressor)
 
+# Random Forest regression
+dataset = read.csv('Data.csv')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$PE, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+#install.packages('randomForest')
+library(randomForest)
+
+rf_regressor = randomForest(x = training_set[-5],
+                         y = training_set$PE,
+                         ntree = 500)
+y_pred = predict(object = rf_regressor, newdata = test_set)
+summary(rf_regressor)
 
 
-
+#from the predictions on the test set, it is evident that Random forest regressor with n = 500 trees
+#is preforming best
