50d374df519baa3ab262a1bd1e79bc786f95c8b9
mkawsarzaman <mkawsarzaman@mun.ca>
2021-10-09 18:59:19 -0230

    initializing and installing caTools package

--- regression_models_comparison_power_plant_data.R
+++ regression_models_comparison_power_plant_data.R
@@ -1,1 +1,17 @@
 # Comparison of regression models in R
+
+# Importing the dataset
+dataset = read.csv('Data.csv')
+
+# Splitting the dataset into training and test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$PE, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+lin_reg = lm(formula = PE ~ .,
+             data = dataset)
+y_pred = predict(object = lin_reg, newdata = test_set)
+summary(lin_reg)
